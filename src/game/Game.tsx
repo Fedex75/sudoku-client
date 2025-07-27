a9bcb978a6bf7cc5eff39f0469565a71677db792
Fedex75 <mezafabrello@gmail.com>
2025-07-27 15:33:09 -0300

    Allow link mode with no cell selected

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -122,7 +122,7 @@ function Game({ paused, handleComplete, game, handleFullNotation }: Props) {
 
         if (colorMode) {
             setMagicWandMode('clearColors');
-        } else if (game.selectedCells.size === 1 && (lockedInput !== 0 || [...game.selectedCells][0].value > 0)) {
+        } else if (lockedInput !== 0 || (game.selectedCells.size === 1 && [...game.selectedCells][0].value > 0)) {
             setMagicWandMode('links');
         } else {
             let selectedColors: ColorName[] = [];
